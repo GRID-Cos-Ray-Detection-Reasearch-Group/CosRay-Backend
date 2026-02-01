c9a5ffaa918181c62308730c7443b5ddada87f7d
travellerse <travellers0238@outlook.com>
2026-02-02 02:51:02 +0800

    feat: 配置 Unfold 后台管理界面

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,62 @@
-# Register your models here.
+from django.contrib import admin
+from unfold.admin import ModelAdmin
+
+from .models import Detector
+
+
+@admin.register(Detector)
+class DetectorAdmin(ModelAdmin):  # type: ignore[misc]
+    """探测器管理后台"""
+
+    list_display = [
+        "mac_address",
+        "name",
+        "owner",
+        "is_active",
+        "last_seen_at",
+        "created_at",
+    ]
+
+    list_filter = [
+        "is_active",
+        "created_at",
+        "last_seen_at",
+    ]
+
+    search_fields = [
+        "mac_address",
+        "name",
+        "owner__username",
+        "owner__email",
+    ]
+
+    readonly_fields = [
+        "created_at",
+        "updated_at",
+    ]
+
+    fieldsets = (
+        (
+            "基本信息",
+            {
+                "fields": ("mac_address", "name", "owner", "description"),
+            },
+        ),
+        (
+            "状态",
+            {
+                "fields": ("is_active", "last_seen_at"),
+            },
+        ),
+        (
+            "时间信息",
+            {
+                "fields": ("created_at", "updated_at"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    def get_queryset(self, request):  # type: ignore[no-untyped-def]
+        qs = super().get_queryset(request)
+        return qs.select_related("owner")
